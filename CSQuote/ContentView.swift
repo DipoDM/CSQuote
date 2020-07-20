3d6b33e35d3da670c2f2b7129000febb091a2386
ODM <danmusad@gmail.com>
2020-07-20 01:12:31 -0500

    Initial Commit

---
+++ CSQuote/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  CSQuote
+//
+//  Created by ODM on 7/20/20.
+//  Copyright © 2020 ODM. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
